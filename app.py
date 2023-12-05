52eb65c983c9b0856b10f909e9be05a6197a7deb
Monica Granbois <MonicaG@users.noreply.github.com>
2023-12-05 10:02:22 -0800

    changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello():
 
 @app.route('/bye')
 def hello():
-    return '<h1><center>Goodbye!</center></h1>'
+    return '<h1><center>Goodbye! More changes!</center></h1>'
 
 @app.route('/test')
 def test():
