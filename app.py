4fbde4e0fdfed7ed1c10e7e5f50ea22d0a66e81a
Monica Granbois <MonicaG@users.noreply.github.com>
2024-10-24 15:25:42 -0700

    testing

--- app.py
+++ app.py
@@ -4,22 +4,27 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '<h1><center>Example app</center></h1>'
+    return '<h1><center>Example App</center></h1>'
+
 
 @app.route('/hello')
 def hello():
     return '<h1><center>Hello! Hello!</center></h1>'
 
+
 @app.route('/bye')
 def hello():
     return '<h1><center>Goodbye!</center></h1>'
+
 
 @app.route('/test')
 def test():
     word = environ.get("APP_WORD")
     return f'<h1><center>Today\'s word is {escape(word)}</h1>'
+
 
 if __name__ == "__main__":
     app_port = environ.get("APP_PORT")
