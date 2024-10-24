4d7459ed280e1600263b0dd0bdb61337b09811b6
Monica Granbois <MonicaG@users.noreply.github.com>
2024-10-24 15:27:48 -0700

    fix and test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1><center>Example App</center></h1>'
+    return '<h1><center>Example App!</center></h1>'
 
 
 @app.route('/hello')
