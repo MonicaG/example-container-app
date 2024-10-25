5c90da6388ef7e1e009c207a8b2c45f741ee2956
Monica Granbois <MonicaG@users.noreply.github.com>
2024-10-25 09:49:38 -0700

    update for testing

--- app.py
+++ app.py
@@ -10,17 +10,14 @@ def index():
     return '<h1><center>Example App</center></h1>'
 
 
-
 @app.route('/hello')
 def hello():
-    return '<h1><center>Hello!</center></h1>'
-
+    return '<h1><center>Hello! Bonjour!</center></h1>'
 
 
 @app.route('/bye')
 def hello():
     return '<h1><center>Goodbye! So long!</center></h1>'
-
 
 
 @app.route('/test')
