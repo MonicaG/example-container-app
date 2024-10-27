5880642e4031ae0a07faa2efe9fe5dba520a1d05
Monica Granbois <MonicaG@users.noreply.github.com>
2024-10-27 15:52:38 -0700

    more testing

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello():
 
 @app.route('/bye')
 def hello():
-    return '<h1><center>Goodbye! Bye! work!</center></h1>'
+    return '<h1><center>Goodbye! Bye! work! work!</center></h1>'
 
 
 @app.route('/test')
